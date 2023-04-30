0b8b6042cd553484412e008066d5b85545f6467e
Kadu <moreira.carloseduardo@gmail.com>
2023-04-30 12:34:13 -0300

    Economy #9

--- Assets/Scripts/Engine/Rule_.cs
+++ Assets/Scripts/Engine/Rule_.cs
@@ -7,9 +7,20 @@ public enum States { Start, Game, End, Win, Lose };
 
 public class Rule_ : MonoBehaviour {
     public StateMachine<States> fsm;
+    private int coins;
+    public int Coins {
+        get { return coins; }
+        set {
+            coins = value;
+            UpdateCoinsUI();
+        }
+    }
 
     void Awake() {
         fsm = StateMachine<States>.Initialize(this);
         fsm.ChangeState(States.Game);
+    }
+    private void UpdateCoinsUI() {
+        Game_.instance.ui.coinsTotalText.text = coins.ToString();
     }
 }
